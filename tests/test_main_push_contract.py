0f1b4dcf55eeb372ac255c8b96510a95bb611ddf
hat47x <hat47x@gmail.com>
2026-09-03 14:32:25 +0900

    test: require exact two-parent main merge shape

--- tests/test_main_push_contract.py
+++ tests/test_main_push_contract.py
@@ -6,13 +6,14 @@ from scripts.check_main_push_contract import MainPushContractError, check_main_p
 
 
 class MainPushContractTests(unittest.TestCase):
-    def test_accepts_two_parent_merge_commit(self) -> None:
+    def test_accepts_two_parent_merge_commit_shape(self) -> None:
         check_main_push_parents("a" * 40 + " " + "b" * 40)
 
-    def test_accepts_merge_commit_with_more_than_two_parents(self) -> None:
-        check_main_push_parents("a b c")
+    def test_rejects_merge_commit_with_more_than_two_parents(self) -> None:
+        with self.assertRaises(MainPushContractError):
+            check_main_push_parents("a b c")
 
-    def test_rejects_single_parent_direct_push(self) -> None:
+    def test_rejects_single_parent_commit(self) -> None:
         with self.assertRaises(MainPushContractError):
             check_main_push_parents("a" * 40)
 
